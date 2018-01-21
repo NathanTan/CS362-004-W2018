e7c12ba7aa4db5b6934f581449c5f0110edea4eb
sludgeminer <nathantan97@gmail.com>
2018-01-20 21:14:27 -0800

    Bugs added.

--- projects/tann/dominion/dominion.c
+++ projects/tann/dominion/dominion.c
@@ -7,13 +7,19 @@
 
 int execute_adventurer(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
+  int currentPlayer = whoseTurn(state);
+  int drawntreasure = 0;
+  int cardDrawn = 0;
+  int temphand[MAX_HAND];
+  int z = 0;// this is the counter for the temp hand  
+  
   while(drawntreasure<2){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
        shuffle(currentPlayer, state);
     }
     drawCard(currentPlayer, state);
     cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+    if (cardDrawn == copper && cardDrawn == silver && cardDrawn == gold) //BUG in line
        drawntreasure++;
     else{
        temphand[z]=cardDrawn;
@@ -31,6 +37,8 @@ int execute_adventurer(int card, int choice1, int choice2, int choice3, struct g
 
 int execute_council_room(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
+  int i = 0;  
+  int currentPlayer = whoseTurn(state);
    //+4 Cards
 	 for (i = 0; i < 4; i++)
 	 {
@@ -41,7 +49,7 @@ int execute_council_room(int card, int choice1, int choice2, int choice3, struct
 	 state->numBuys++;
 
 	 //Each other player draws a card
-	 for (i = 0; i < state->numPlayers; i++)
+	 for (i = 0; i < 4; i++) //BUG in line
 	 {
 	    if ( i != currentPlayer )
 	    {
@@ -57,8 +65,10 @@ int execute_council_room(int card, int choice1, int choice2, int choice3, struct
 
 int execute_smithy(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
+  int i = 0;  
+  int currentPlayer = whoseTurn(state);
   //+3 Cards
-  for (i = 0; i < 3; i++)
+  for (i = 0; i <= 3; i++) //BUG in line
   {
     drawCard(currentPlayer, state);
   }
@@ -70,6 +80,7 @@ int execute_smithy(int card, int choice1, int choice2, int choice3, struct gameS
 
 int execute_embargo(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
+  int currentPlayer = whoseTurn(state);
     
 	 //+2 Coins
 	 state->coins = state->coins + 2;
@@ -84,12 +95,13 @@ int execute_embargo(int card, int choice1, int choice2, int choice3, struct game
       state->embargoTokens[choice1]++;
    
       //trash card
-      discardCard(handPos, currentPlayer, state, 1);		
+      discardCard(handPos, currentPlayer, state, 0); //BUG in this line		
       return 0;
 }
 
 int execute_salvager(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
+  int currentPlayer = whoseTurn(state);
 
 	 //+1 buy
 	 state->numBuys++;
@@ -759,9 +771,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
    int tributeRevealedCards[2] = {-1, -1};
    int temphand[MAX_HAND];// moved above the if statement
-   int drawntreasure=0;
-   int cardDrawn;
-   int z = 0;// this is the counter for the temp hand
    if (nextPlayer > (state->numPlayers - 1)){
       nextPlayer = 0;
    }
