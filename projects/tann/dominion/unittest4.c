f0024477c9cfff8cb59806a1e70a9324aab53a7f
Nathan Tan <tann@oregonstate.edu>
2018-02-03 20:26:42 -0800

    Full deck count test good to go.

--- projects/tann/dominion/unittest4.c
+++ projects/tann/dominion/unittest4.c
@@ -1,12 +1,44 @@
 #include <assert.h>
 #include <stdio.h>
+#include <math.h>
+#include "dominion.h"
 
-int main() {
+int main()
+{
+    char *testFunction = "fullDeckCount";
+    int seed = 4;
+    int numPlayers = 3;
+    struct gameState state;
+    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+                 sea_hag, tribute, smithy, council_room};
+
+    // initialize a game state
+    initializeGame(numPlayers, k, seed, &state);
+
+    printf("----------------Testing: %s----------------\n", testFunction);
+
+    /*----------------------Test 1----------------------*/
+    int testPlayer = 0;
+    int card = copper; 
+    int deckCount = fullDeckCount(testPlayer, card, &state);
+    printf("deck count: %d\n", deckCount);
+    assert(deckCount == 7); //Player starts with 7 copper cards.
+    
+    /*----------------------Test 2----------------------*/
+    testPlayer = 0;
+    card = silver; 
+    deckCount = fullDeckCount(testPlayer, card, &state);
+    printf("deck count: %d\n", deckCount);
+    assert(deckCount == 0); //Player starts with 0 silver cards.
+
+    /*----------------------Test 2----------------------*/
+    testPlayer = 0;
+    card = curse; 
+    deckCount = fullDeckCount(testPlayer, card, &state);
+    printf("deck count: %d\n", deckCount);
+    assert(deckCount == 0); //Player starts with 0 curse cards.
+    
 
 
-
-
-
-
-
+    return 0;
 }
